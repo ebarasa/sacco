99deae1e32c4120e53fe1bc0b9532b4027fbc562
eddie barasa <eddiebarasa.20@gmail.com>
2019-03-10 20:37:14 +0300

    initial

--- users/models.py
+++ users/models.py
@@ -2,17 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Owner(models.Model):
+class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-	name = models.CharField()
+
 	def __str__(self):
 		return f'{self.user.username} Profile'
 
-class Matatu(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	name=models.CharField()
-	name=models.CharField()
+class Details(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 class Activities(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
